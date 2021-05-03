0a7eebf62b9c1f4611fcb21ec2f35766fe9657e1
susithrupasinghe <susith03program@gmail.com>
2021-05-04 03:05:16 +0530

    Header and footer fixed

--- res/staff/login.php
+++ res/staff/login.php
@@ -5,21 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SORIS Help Desk</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+    <link rel="stylesheet" href="../../css/style.css">
     <link href="http://fonts.cdnfonts.com/css/sitara" rel="stylesheet">
 
 </head>
 
 <body>
-    <?php include("res/templates/header.php");  ?>
+    <?php include("../templates/header.php");  ?>
 
     <div class="body-container">
 
 
     </div>
-    
-    <?php include("res/templates/footer.php");  ?>
+
+    <?php include("../templates/footer.php");  ?>
 </body>
 
 </html>
--- res/templates/footer.php
+++ res/templates/footer.php
@@ -1,6 +1,6 @@
 <div>
     <div class="footer">
-        <div style="float: left; margin-top:20px; margin-left:50px;">
+        <div style="float:left;padding-left:50px;">
             <h5 class="txt-white" style="font-family: 'Sitara', sans-serif;">Assocoated Links/ Portals</h5>
 
             <ul class="footer-list">
@@ -10,7 +10,7 @@
             </ul>
         </div>
 
-        <div style="float: right; margin-top:20px; margin-right:50px;">
+        <div style="float:right;padding-right:50px;">
             <h5 class="txt-white" style="font-family: 'Sitara', sans-serif; font-size:small;">Assocoated Links/ Portals</h5>
 
             <ul class="footer-list">
@@ -19,6 +19,12 @@
                 <li><a href="http://" target="_blank">SORIS online lecture portal</a></li>
             </ul>
         </div>
-        <p class="txt-white" style="font-size:small;bottom:0;position:absolute;padding-left:40vw;">Developed by SORIS IT devision. All rights reserved 2021</p>
+
+        
+       
     </div>
+   <div class="footer">
+   <p class="txt-white" style="font-size:small;padding-left:40vw;">Developed by SORIS IT devision. All rights reserved 2021</p>
+   </div>
+   
 </div>
