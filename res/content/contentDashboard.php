e2171535902dd9c93f3bd77153dad37d56b6012b
susithrupasinghe <susith03program@gmail.com>
2021-05-14 18:10:34 +0530

    Table css added

---
+++ res/content/contentDashboard.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+    <div class="body-container">
+        <table class="table-style">
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>COntent</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+
+</body>
+
+</html>
