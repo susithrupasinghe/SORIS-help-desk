318ae63dfb19f049288464f4f42e7bc58cba36b7
EMSDEkanayake <it20631192@my.sliit.lk>
2021-05-19 21:08:53 +0530

    Completed new password page

--- res/others/newPassword.php
+++ res/others/newPassword.php
@@ -42,7 +42,7 @@ closeCon($con);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SORIS Help Desk</title>
+    <title>SORIS Help Desk/New Password</title>
 
     <link rel="stylesheet" href="../../css/style.css">
 
