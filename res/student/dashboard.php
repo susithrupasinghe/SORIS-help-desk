fdfde00385bd0b1aa24599db6ddfd54892c6e607
Ravindu Anjana <80050358+Ravindu-Anjana@users.noreply.github.com>
2021-05-28 13:50:33 +0530

    changed student dashboard title

--- res/student/dashboard.php
+++ res/student/dashboard.php
@@ -22,7 +22,7 @@ else
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SORIS Student-dashboard</title>
+    <title>Student Dashboard</title>
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="icon" href="../../images/favicon.svg" sizes="any" type="image/svg+xml">
     <link href="http://fonts.cdnfonts.com/css/sitara" rel="stylesheet">
