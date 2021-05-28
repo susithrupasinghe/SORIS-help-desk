5ee6847a38a6fcc8795c6c8eea44424e34fb0075
Indhuwaree <it20630270@my.sliit.lk>
2021-05-28 20:33:20 +0530

    Change title name

--- res/admin/dashboard.php
+++ res/admin/dashboard.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['userid']) && isset($_SESSION['role'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SORIC adminDashboard</title>
+    <title>SORIC Administrator Dashboard</title>
 
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/signup.css">
