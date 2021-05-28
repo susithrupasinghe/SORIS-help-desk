1a4f8dd3dee0d7bfaf01d44c5774f5f9bd97ece1
EMSDEkanayake <it20631192@my.sliit.lk>
2021-05-28 20:32:30 +0530

    Change title of relavent pages

--- res/staff/dashboard.php
+++ res/staff/dashboard.php
@@ -19,7 +19,7 @@ if (isset($_SESSION["userid"]) && isset($_SESSION["role"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SORIS Help Desk</title>
+    <title>Staff Dashboard</title>
     <link rel="stylesheet" href="../../css/style.css">
 
     <link rel="icon" href="../../images/favicon.svg" sizes="any" type="image/svg+xml">
