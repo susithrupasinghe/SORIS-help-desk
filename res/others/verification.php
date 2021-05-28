9ac211f2957087e154e9995070833bdcec0bc2be
OSANDA1999 <IT20633554@my.sliit.lk>
2021-05-28 20:13:04 +0530

    Add icon  for verification page

--- res/others/verification.php
+++ res/others/verification.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <link rel="icon" href="../../images/favicon.svg" sizes="any" type="image/svg+xml">
     <link rel="stylesheet" href="../../css/style.css">
 
 </head>
