dd6602616f896c5d8afe90acc7bd01b14b141598
OSANDA1999 <IT20633554@my.sliit.lk>
2021-05-29 00:04:19 +0530

    Change fonts and add required

--- res/others/verification.php
+++ res/others/verification.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verification</title>
-    <link rel="icon" href="../../images/favicon.svg" sizes="any" type="image/svg+xml">
     <link rel="stylesheet" href="../../css/style.css">
-
+    <link rel="icon" href="../../images/favicon.svg" sizes="any" type="image/svg+xml">
+    <link href="http://fonts.cdnfonts.com/css/sitara" rel="stylesheet">
 </head>
 
 <body>
