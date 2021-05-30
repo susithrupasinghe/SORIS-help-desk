5d18809da6a2d725a6b8e999cdc7fe835b5ea909
susithrupasinghe <susith03program@gmail.com>
2021-05-30 15:55:01 +0530

    Conversation title changed

--- res/staff/conversation.php
+++ res/staff/conversation.php
@@ -222,7 +222,7 @@ function message($name, $date, $text, $attachment, $role)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Conversation</title>
 
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="icon" href="../../images/favicon.svg" sizes="any" type="image/svg+xml">
